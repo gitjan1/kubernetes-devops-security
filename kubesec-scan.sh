7ece0d8cd3c4d714375c1bf8320a36860c2977dd
gitjan1 <jana.oem1@gmail.com>
2024-05-29 18:07:04 +0530

    Update kubesec-scan.sh

--- kubesec-scan.sh
+++ kubesec-scan.sh
@@ -7,9 +7,9 @@ scan_message=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deplo
 scan_score=$(docker run -i kubesec/kubesec:512c5e0 scan /dev/stdin < k8s_deployment_service.yaml | jq .[].score)
 
 # Kubesec scan result processing
-    # echo "Scan Score : $scan_score"
+    echo "Scan Score : $scan_score"
 
-	if [ "${scan_score}" -le 1 ]; then
+	if [ ${scan_score} -le 1 ]; then
 	    echo "Score is $scan_score"
 	    echo "Kubesec Scan $scan_message"
 	else
